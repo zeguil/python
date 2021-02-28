14ff8739115f60a22ad7686f94667620458c3a06
José Guilherme <61730073+zeguil@users.noreply.github.com>
2021-02-28 03:43:07 -0300

    Update questao9.py

--- questao9.py
+++ questao9.py
@@ -1,8 +1,9 @@
+# Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+
 num1 = int(input("digite um numero: "))
 num2 = int(input("digite outro numero: "))
 
 lista = []
-
 
 while (num2 < num1):
 	print ("\n#  ERROR: primeiro numero deve ser maior que o segundo. #\n")
